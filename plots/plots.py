53314db9fddddf4220d35e00b6b89d64bfad220f
ARYAMAN NAGPAL <an3782@nyu.edu>
2025-06-20 22:15:54 -0400

    plots v1

--- plots/plots.py
+++ plots/plots.py
@@ -2,4 +2,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 fxrates = pd.read_csv('/Users/aryaman/macro-research/data/fxrates.csv', index_col=0, parse_dates=True)
-print(fxrates.head(5))
+
+plt.figure(figsize=(10, 5))
+fxrates['exukus'].plot(label='GBP/USD', color='orange')
+plt.title('GBP/USD')
+plt.xlabel('Date')
+plt.ylabel('Exchange Rate')
+plt.legend()
+plt.tight_layout()
+plt.savefig('/Users/aryaman/macro-research/plots/figures/gbp-usd.png')
+plt.show()
+
+plt.figure(figsize=(10, 5))
+fxrates['exchus'].dropna().plot(label='YUAN/USD', color='orange')
+plt.title('YUAN/USD')
+plt.xlabel('Date')
+plt.ylabel('Exchange Rate')
+plt.legend()
+plt.tight_layout()
+plt.savefig('/Users/aryaman/macro-research/plots/figures/yuan-usd.png')
+plt.show()
+
+plt.figure(figsize=(10, 5))
+fxrates['exeuus'].dropna().plot(label='EUR/USD', color='orange')
+plt.title('EUR/USD')
+plt.xlabel('Date')
+plt.ylabel('Exchange Rate')
+plt.legend()
+plt.tight_layout()
+plt.savefig('/Users/aryaman/macro-research/plots/figures/eur-usd.png')
+plt.show()
+
+plt.figure(figsize=(10, 5))
+fxrates['exjpus'].plot(label='YEN/USD', color='orange')
+plt.title('YEN/USD')
+plt.xlabel('Date')
+plt.ylabel('Exchange Rate')
+plt.legend()
+plt.tight_layout()
+plt.savefig('/Users/aryaman/macro-research/plots/figures/yen-usd.png')
+plt.show()
+
+
