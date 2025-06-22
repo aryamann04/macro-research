37a8935a7ff5379c39a825499633d04ee2dd3cdf
aryamann04 <aryaman.nagpal04@gmail.com>
2025-06-22 14:47:24 -0400

    redo fx plots

--- plots/fxrates.py
+++ plots/fxrates.py
@@ -1,53 +1,61 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-# load data, truncate data to start from 1991-01-01
+# load data
 
-cutoff_date = '1991-01-01'
+usd_yen = pd.read_csv('/Users/aryaman/macro-research/data/usd-yen.csv', index_col=0, parse_dates=True)
+usd_eur = pd.read_csv('/Users/aryaman/macro-research/data/usd-eur.csv', index_col=0, parse_dates=True)
+usd_gbp = pd.read_csv('/Users/aryaman/macro-research/data/usd-gbp.csv', index_col=0, parse_dates=True)
+usd_yuan = pd.read_csv('/Users/aryaman/macro-research/data/usd-yuan.csv', index_col=0, parse_dates=True)
 
-fxrates = pd.read_csv('/Users/aryaman/macro-research/data/fxrates.csv', index_col=0, parse_dates=True)
-fxrates = fxrates.truncate(before=cutoff_date)
+fxrates = pd.DataFrame()
+fxrates['USD/YEN'] = usd_yen['CCUSMA02JPM618N']
+fxrates['USD/EUR'] = usd_eur['CCUSMA02EZM618N']
+fxrates['USD/GBP'] = usd_gbp['CCUSMA02GBM618N']
+fxrates['USD/YUAN'] = usd_yuan['CCUSMA02CNM618N']
+
+print(fxrates.head(10))
 
 # plots 
 
 plt.figure(figsize=(10, 5))
-fxrates['exukus'].plot(label='GBP/USD', color='orange')
-plt.title('GBP/USD')
+fxrates['USD/GBP'].plot(label='USD/GBP', color='orange')
+plt.title('USD/GBP')
 plt.xlabel('date')
-plt.ylabel('exchange rate')
+plt.ylabel('pound sterling per USD')
 plt.legend()
 plt.tight_layout()
-plt.savefig('/Users/aryaman/macro-research/plots/figures/gbp-usd.png')
+plt.savefig('/Users/aryaman/macro-research/plots/figures/usd-gbp.png')
 plt.show()
 
 plt.figure(figsize=(10, 5))
-fxrates['exchus'].dropna().plot(label='YUAN/USD', color='orange')
-plt.title('YUAN/USD')
+fxrates['USD/YUAN'].dropna().plot(label='USD/YUAN', color='orange')
+plt.title('USD/YUAN')
 plt.xlabel('date')
-plt.ylabel('exchange rate')
+plt.ylabel('yuan reminbi per USD')
 plt.legend()
 plt.tight_layout()
-plt.savefig('/Users/aryaman/macro-research/plots/figures/yuan-usd.png')
+plt.savefig('/Users/aryaman/macro-research/plots/figures/usd-yuan.png')
 plt.show()
 
 plt.figure(figsize=(10, 5))
-fxrates['exeuus'].dropna().plot(label='EUR/USD', color='orange')
-plt.title('EUR/USD')
+fxrates['USD/EUR'].dropna().plot(label='USD/EUR', color='orange')
+plt.title('USD/EUR')
 plt.xlabel('date')
-plt.ylabel('exchange rate')
+plt.ylabel('euro per USD')
 plt.legend()
 plt.tight_layout()
-plt.savefig('/Users/aryaman/macro-research/plots/figures/eur-usd.png')
+plt.savefig('/Users/aryaman/macro-research/plots/figures/usd-eur.png')
 plt.show()
 
 plt.figure(figsize=(10, 5))
-fxrates['exjpus'].plot(label='YEN/USD', color='orange')
-plt.title('YEN/USD')
+fxrates['USD/YEN'].plot(label='USD/YEN', color='orange')
+plt.title('USD/YEN')
 plt.xlabel('date')
-plt.ylabel('exchange rate')
+plt.ylabel('yen per USD')
 plt.legend()
 plt.tight_layout()
-plt.savefig('/Users/aryaman/macro-research/plots/figures/yen-usd.png')
+plt.savefig('/Users/aryaman/macro-research/plots/figures/usd-yen.png')
 plt.show()
 
 
